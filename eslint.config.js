d27251c9bf7e74ad2e1a05047881956f1743f11e
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-08-14 02:46:40 +1000

    chore: config eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default [
+  { files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+export default {
+  singleQuote: false,
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
+};
